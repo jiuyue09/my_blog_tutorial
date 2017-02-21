1a62ad3a005a8bc11cc46c36069a79c4bc3ccc07
马胜骁 <Kicheal@mashengxiaodeMacBook-Air.local>
2017-02-21 16:18:54 +0800

    修改

--- article/views.py
+++ article/views.py
@@ -7,11 +7,13 @@ def home(request):
 	post_list = Article.objects.all();
 	return render(request, 'home.html',{'post_list' : post_list})
 
-def detail(request, my_args):
+def detail(request, id):
 #	    post = Article.objects.all()[int(my_args)]
-	post = Article.objects.all()[0]
-	str = ("title = %s, category = %s, data_time = %s, content = %s" % (post.title, post.category, post.date_time, post.content))
-	return HttpResponse(str)
+	try:
+		post = Article.objects.get(id=str(id))
+	except Article.DoesNotExist:
+		raise Http404
+	return render(request, 'home.html',{'post' : post})
 
 def test(request):
 	return render(request, 'test.html', {'current_time':datetime.now()})
